df0e814e1411836b6f01a3f7582de1f40e103df9
Ryan567dot <itsbluey1010@gmail.com>
2024-10-23 21:55:51 +0600

    polymorph.py

---
+++ polymorph.py
@@ -0,0 +1,11 @@
+class Mercedes:
+    def speed(self):
+        print("Mercedes max speed: 240 MP/H")
+        
+class BMW:
+    def speed(self):
+        print("BMW max speed: 225 MP/H")
+        
+class Lambo:
+    def speed(self):
+        print("Lambo max speed: 330 MP/h")
